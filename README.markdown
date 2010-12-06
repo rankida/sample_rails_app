09f508b0eeede8199571006ef75a5d176775d1a6
David Rankin <Rankida@gmail.com>
2010-12-06 20:35:47 +0000

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,8 @@
+# Ruby on Rails Tutorial: second application
+
+This is the second application from
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/) by [Micheal Hart](http://michaelhart.com/).
+
+This is the work from Chapter 3 onwards.
+
+This is David Rankin's attempt at the tutorial (don't laugh too much)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'caf9d0a36bcae9ff7ffbebd5f26710ea8c4f672e1fef1410b4ed3aca1086f37d6e5db053255d33fa0bea6fe0fae03702b3f87693409120ea34153b9b21fa5ce6'
