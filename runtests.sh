aad40df35999a7a7558c3a4a16677b57200a8b3f
David Rankin <Rankida@gmail.com>
2011-02-27 14:10:15 +0000

    Completed section 3.3.2 of the tutorial. Now have page titles being tested, thought everything is very much still static

---
+++ runtests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "This is my first bash script in over 3 years"
+echo "My hope is that this bash script can be used to launch"
+echo " - spork - to host my rails code in memeory for faster tests"
+echo " - autotest - to run my specs automatically"
+
+spork
+autotest
+
+echo "To stop hit Ctrl + c"
